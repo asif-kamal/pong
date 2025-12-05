79518a4635005d0c93eae50af112d7d85bd0448a
asif-kamal <asif.kamal1489@gmail.com>
2025-12-04 19:26:49 -0600

    now able to move the right paddle up and down

--- Paddles.py
+++ Paddles.py
@@ -1,27 +1,24 @@
 import turtle
 
 COORDINATES = ((50, 10), (-50, 10), (-50, -10), (50, -10))
-X = 375
-Y = 0
+
 
 class Paddles(turtle.Turtle):
     def __init__(self):
         super().__init__()
         turtle.register_shape("rectangle", COORDINATES)
         self.penup()
-        self.goto(X, Y)
+        self.goto(375, 0)
         self.shape("rectangle")
         self.color("white", "white")
 
 
-    def move_right_paddle_up(self, screen):
-        if -275 < Y < 275:
-            self.goto(X, Y + 20)
-            screen.update()
+    def move_right_paddle_up(self, y_change):
+        if self.ycor() < 275:
+            self.goto(self.xcor(), self.ycor() + y_change)
 
-    def move_right_paddle_down(self, screen):
-        if -275 < self.y < 275:
-            self.goto(X, Y - 10)
-            screen.update()
+    def move_right_paddle_down(self, y_change):
+        if self.ycor() > -275:
+            self.goto(self.xcor(), self.ycor() - y_change)
 
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import turtle
 import Paddles
 
@@ -6,11 +7,14 @@ main_screen = turtle.Screen()
 main_screen.bgcolor("black")
 main_screen.setup(width=800, height=600)
 main_screen.title("Pong")
-main_screen.tracer(0)
 
-main_screen.listen()
+game_is_on = True
 right_paddle = Paddles.Paddles()
+main_screen.listen()
+main_screen.onkey(lambda: right_paddle.move_right_paddle_up(10), "Up")
+main_screen.onkey(lambda: right_paddle.move_right_paddle_down(10), "Down")
 
-main_screen.onkeypress(right_paddle.move_right_paddle_up(main_screen), "Up")
+while game_is_on:
+    main_screen.update()
 
 main_screen.exitonclick()
